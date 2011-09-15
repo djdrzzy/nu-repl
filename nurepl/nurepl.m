0923dc591e2832900c9f9cb8aa9b52f2ebd52700
Daniel Drzimotta <djdrzzy@gmail.com>
2011-09-15 00:40:51 -0600

    Added a tentative static library to link against... be wary as untested. Going to bed though!

---
+++ nurepl/nurepl.m
@@ -0,0 +1,23 @@
+//
+//  nurepl.m
+//  nurepl
+//
+//  Created by Daniel Drzimotta on 11-09-15.
+//  Copyright 2011 Soma Creates. All rights reserved.
+//
+
+#import "nurepl.h"
+
+@implementation nurepl
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
